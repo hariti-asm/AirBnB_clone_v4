9e9f154367186374e722dd04b717a26858b3b452
salihabrik <briksaliha@gmail.com>
2023-11-03 20:10:25 +0100

    UPDATe

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -41,13 +41,8 @@ def hbnb():
     return render_template('100-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places
-                           cache_id=cache_id)  # Pass the cache_id as a variable
-
-
-
-
-
+                           places=places,
+                           cache_id=cache_id),
 
 if __name__ == "__main__":
     """ Main Function """
