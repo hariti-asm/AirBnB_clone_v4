ed84d7e02926e5a2a1bf7a8ba6b609f557c0afb0
salihabrik <briksaliha@gmail.com>
2023-11-04 23:06:29 +0100

    API status

---
+++ web_dynamic/static/scripts/ 2-hbnb.js
@@ -0,0 +1,15 @@
+// Wait for the document to be fully loaded
+$('document').ready(function () {
+    // Define the URL of the API endpoint
+    const url = 'http://0.0.0.0:5001/api/v1/status/';
+
+    // Send a GET request to the API endpoint
+    $.get(url, function (data) {
+        if (data.status === 'OK') {
+            // If the status is "OK," add the class "available" to the element with id "api_status"
+            $('#api_status').addClass('available');
+        } else {
+            $('#api_status').removeClass('available');
+        }
+    });
+});
